531dc2df030d680409760e1ac2b84625cddc913a
Vinayak Aggarwal <f20170008@goa.bits-pilani.ac.in>
2020-04-04 17:43:38 +0530

    udpate item gst

--- Backend/hackcrisis/sellershop/views.py
+++ Backend/hackcrisis/sellershop/views.py
@@ -73,7 +73,7 @@ class ItemList(APIView):
 		itemname = request.POST.get('name')
 		price = request.POST.get('price')
 		quantity_max = request.POST.get('quant')
-		shop = Shop.objects.get(pk=request.POST.get('shop'))
+		shop = Shop.objects.get(gst_no=request.POST.get('gst'))
 		description=request.POST.get('desc')
 		product=Item(itemname=itemname,price=price,description=description,shop=shop,quantity_max=quantity_max)
 		product.save()
