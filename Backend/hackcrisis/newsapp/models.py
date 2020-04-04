616b15729bf88f02446a7fe6aa33ca8ded955f61
Vinayak Aggarwal <f20170008@goa.bits-pilani.ac.in>
2020-04-04 12:20:55 +0530

    Fix Typo

--- Backend/hackcrisis/newsapp/models.py
+++ Backend/hackcrisis/newsapp/models.py
@@ -19,4 +19,4 @@ class Announcements(models.Model):
 	description = models.CharField(max_length = 200)
 
 	def __str__(self):
-			return str(str(self.id) + " " + self.title
+			return str(str(self.id)) + " " + self.title
